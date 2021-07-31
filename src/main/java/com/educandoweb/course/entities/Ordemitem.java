8ab131d3f45bd37f8b98faa626170446d71549f1
Phb29 <paulototototo@gmail.com>
2021-07-31 14:28:33 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/Ordemitem.java
+++ src/main/java/com/educandoweb/course/entities/Ordemitem.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="tb_order_item")
@@ -34,12 +35,14 @@ public class Ordemitem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
 	public void setOrder(Order order) {
 		 id.setOrder(order);
 	}
+	
 	public Product getProduct() {
 		return id.getProduct();
 	}
