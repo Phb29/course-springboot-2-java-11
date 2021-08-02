cb37821159f7246bb636a88a64d770719c6b8cc0
Phb29 <paulototototo@gmail.com>
2021-08-02 10:57:36 -0300

    update user

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
@@ -31,5 +30,17 @@ public User insert(User obj) {
 }
 public void delete(Long id) {
 	repository.deleteById(id);
+}
+public User update(Long id,User obj) {
+	
+	User entity = repository.getOne(id);
+	updateData(entity,obj);
+	return repository.save(entity);
+	
+}
+private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());;
 }
 }
